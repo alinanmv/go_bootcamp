836bb5b3c6a905062f204d929d97bbe6b525ffa9
alinanmv <alinanumanova555@gmail.com>
2026-09-19 23:30:26 +0500

    if statements

--- bank.go
+++ bank.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	accountBalance := 1000.0
 	fmt.Println("WELCOME TO GO BANK")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check Balance")
@@ -11,5 +12,24 @@ func main() {
 	fmt.Println("4. Exit")
 
 	var choice int
+	fmt.Print("Your choice:")
 	fmt.Scan(&choice)
+
+	if choice == 1 {
+		fmt.Println("Account Balance:", accountBalance)
+	} else if choice == 2 {
+		var depositMoney float64
+		fmt.Print("Enter Money for Deposit: ")
+		fmt.Scan(&depositMoney)
+		accountBalance += depositMoney
+		fmt.Println("Updated account balance:", accountBalance)
+	} else if choice == 3 {
+		var withdrawMoney float64
+		fmt.Print("Enter Money for Withdraw: ")
+		fmt.Scan(&withdrawMoney)
+		accountBalance -= withdrawMoney
+		fmt.Println("Updated account balance:", accountBalance)
+	} else {
+		fmt.Println("Goodbye")
+	}
 }
